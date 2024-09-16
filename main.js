2cb688aa06a5143907c8ca1564232a91dd76640d
Felipe <felwal1987@gmail.com>
2024-09-16 14:26:04 -0600

    Added Dyanmic list rendering and Name for each pet

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const temperatureElement = document.querySelector("#temperature");
+const template = document.querySelector("#pet-card-template");
+const wrapper = document.createDocumentFragment();
+const listOfPets = document.querySelector(".list-of-pets");
 
 start = async () => {
   const weatherPromise = await fetch(
@@ -20,9 +23,15 @@ const petsArea = async (url) => {
   const pets = await fetch(url);
   const petsData = await pets.json();
 
+  console.log(petsData);
+
   petsData.forEach((pet) => {
-    console.log(pet.name);
+    const clone = template.content.cloneNode(true);
+    clone.querySelector("h3").textContent = pet.name;
+    wrapper.appendChild(clone);
   });
+
+  listOfPets.appendChild(wrapper);
 };
 
 petsArea("https://learnwebcode.github.io/bootcamp-pet-data/pets.json");
